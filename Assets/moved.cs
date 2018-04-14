8c3c4e4d7df7d751d795fb2d3b8899f5ea264b45
apple861104 <apple861104@gmail.com>
2018-04-14 14:50:41 +0800

    123

---
+++ Assets/moved.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moved : MonoBehaviour {
+
+    public Transform ball;
+
+    Vector3 offset;
+	// Use this for initialization
+	void Start () {
+
+        offset = transform.position - ball.position ;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = ball.position + offset;
+	}
+}
